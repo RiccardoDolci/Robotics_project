0cd72b55992ba623e8ae4014a3e9ec01e58e5bf7
ema1709 <emanuelemoschillo@gmail.com>
2025-04-16 15:28:29 +0200

    Add files via upload

---
+++ src/odometer.cpp
@@ -0,0 +1,107 @@
+// bicycle_odometry_node.cpp
+#include <ros/ros.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Quaternion.h>
+#include <tf/transform_broadcaster.h>
+#include <math.h>
+
+class BicycleOdometryNode {
+public:
+    BicycleOdometryNode() {
+        ros::NodeHandle private_nh("~");
+        private_nh.param("wheelbase", wheelbase_, 1.765);
+        private_nh.param("steering_factor", steering_factor_, 92.0);
+
+        x_ = y_ = 0.0;
+        theta_=M_PI/2;
+        
+        last_time_ = ros::Time::now();
+
+        speedsteer_sub_ = nh_.subscribe("/speedsteer", 1, &BicycleOdometryNode::speedsteerCallback, this);
+        odom_pub_ = nh_.advertise<nav_msgs::Odometry>("/odom", 1);
+    }
+
+private:
+    ros::NodeHandle nh_;
+    ros::Subscriber speedsteer_sub_;
+    ros::Publisher odom_pub_;
+    tf::TransformBroadcaster odom_broadcaster_;
+
+    double wheelbase_;
+    double steering_factor_;
+    double x_, y_, theta_;
+    ros::Time last_time_;
+
+    void speedsteerCallback(const geometry_msgs::PointStamped::ConstPtr& msg) {
+         ROS_INFO("Callback attivato! steer: %f, speed: %f", msg->point.x, msg->point.y);
+
+        double steer_deg = msg->point.x;  // L'angolo di sterzata in gradi (campo x)
+        double v_kmh = msg->point.y;      // La velocità in km/h (campo y)
+
+
+        
+        double v = v_kmh / 3.6;  // Conversione km/h → m/s
+        double alpha = (steer_deg / steering_factor_) * M_PI / 180.0;  // angolo di sterzo in radianti
+
+        ros::Time current_time = ros::Time::now();
+        double dt = (current_time - last_time_).toSec();
+        last_time_ = current_time;
+
+        // Calcolo della velocità angolare
+        //double omega = vf*sin(alpha)/wheelbase_;
+        double omega = (v / wheelbase_) * tan(alpha);
+        //double v = omega * wheelbase_/tan(alpha);
+
+        // Calcolo posizione con RK2 (orientamento medio)
+        double theta_mid = theta_ +  (omega * dt)/2;
+
+        x_ += v * dt * cos(theta_mid);
+        y_ += v * dt * sin(theta_mid);
+        theta_ += omega * dt;
+
+
+        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(theta_);
+
+        nav_msgs::Odometry odom;
+        odom.header.stamp = current_time;
+        odom.header.frame_id = "world";
+        odom.child_frame_id = "vehicle";
+
+        odom.pose.pose.position.x = x_;
+        odom.pose.pose.position.y = y_;
+        odom.pose.pose.position.z = 0.0;
+        odom.pose.pose.orientation = odom_quat;
+
+        odom.twist.twist.linear.x = v;
+        //odom.twist.twist.angular.z = (v / wheelbase_) * tan(alpha);
+        odom.twist.twist.angular.z = omega ;
+
+        odom_pub_.publish(odom);
+
+        geometry_msgs::TransformStamped odom_tf;
+        odom_tf.header.stamp = current_time;
+        odom_tf.header.frame_id = "world";
+        odom_tf.child_frame_id = "vehicle";
+
+        odom_tf.transform.translation.x = x_;
+        odom_tf.transform.translation.y = y_;
+        odom_tf.transform.translation.z = 0.0;
+        odom_tf.transform.rotation = odom_quat;
+
+        odom_broadcaster_.sendTransform(odom_tf);
+    } 
+};
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "odometer");
+    BicycleOdometryNode node;
+    ROS_INFO("Nodo avviato correttamente!");
+
+    ros::spin();
+    return 0;
+}
+
+
+
+
---
+++ src/sector_times.cpp
@@ -0,0 +1,94 @@
+#include "ros/ros.h"
+#include "sensor_msgs/NavSatFix.h"
+#include "first_project/custom.h"
+
+class SectorTimer {
+public:
+    SectorTimer() : private_n("~"), current_sector(1), count(0), v_average(0.0) {
+        gps_sub = n.subscribe("/swiftnav/front/gps_pose", 1, &SectorTimer::gpsCallback, this);
+        speed_sub = n.subscribe("/speedsteer", 1, &SectorTimer::speedCallback, this);
+        pub = n.advertise<first_project::custom>("/sector_times", 10);
+
+        private_n.param("sector1_lat", sec1_lat , 45.630106);
+        private_n.param("sector1_longit", sec1_longit, 9.289490);
+
+        private_n.param("sector2_lat", sec2_lat , 45.623570);
+        private_n.param("sector2_longit", sec2_longit, 9.287297);
+
+        private_n.param("sector3_lat", sec3_lat , 45.616042);
+        private_n.param("sector3_longit", sec3_longit, 9.280767);
+
+        last_time = ros::Time::now();
+    }
+
+private:
+    ros::NodeHandle n;
+    ros::NodeHandle private_n;
+    ros::Subscriber gps_sub;
+    ros::Subscriber speed_sub;
+    ros::Publisher pub;
+
+    double sec1_lat, sec1_longit;
+    double sec2_lat, sec2_longit;
+    double sec3_lat, sec3_longit;
+    ros::Time last_time;
+    double v_average;
+    int count;
+    int current_sector;
+    double current_speed;
+
+    const double tol = 0.00005; // tolleranza per confronto coordinate
+
+    void speedCallback(const geometry_msgs::PointStamped::ConstPtr& msg) {
+        current_speed = msg->point.y;
+    }
+
+    void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
+        double lat = msg->latitude;
+        double longit = msg->longitude;
+        ros::Time now = ros::Time::now();
+
+        // Settore 1
+        if (std::abs(lat - sec1_lat) < tol && std::abs(longit - sec1_longit) < tol) {
+            if (current_sector != 1) {
+                last_time = now;
+                v_average = 0;
+                count = 0;
+                current_sector = 1;
+                ROS_INFO("Entrato nel settore 1");
+            }
+        }
+        // Settore 2
+        else if (std::abs(lat - sec2_lat) < tol && std::abs(longit - sec2_longit) < tol) {
+            if (current_sector != 2) {
+                last_time = now;
+                v_average = 0;
+                count = 0;
+                current_sector = 2;
+                ROS_INFO("Entrato nel settore 2");
+            }
+        }
+        // Settore 3
+        else if (std::abs(lat - sec3_lat) < tol && std::abs(longit - sec3_longit) < tol) {
+            if (current_sector != 3) {
+                last_time = now;
+                v_average = 0;
+                count = 0;
+                current_sector = 3;
+                ROS_INFO("Entrato nel settore 3");
+            }
+        }
+
+        // Calcolo tempo e velocità media
+        count++;
+        v_average = ((v_average * (count - 1)) + current_speed) / count;
+
+        first_project::custom msg_out;
+        msg_out.current_sector = current_sector;
+        msg_out.current_sector_time = (now - last_time).toSec();
+        msg_out.current_sector_mean_speed = v_average;
+
+        pub.publish(msg_out);
+    }
+};
+
