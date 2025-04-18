d5abbd670799bf9e87db490e5b519b8a129db215
ema1709 <emanuelemoschillo@gmail.com>
2025-04-18 09:33:40 +0200

    Update sector_times.cpp

--- src/sector_times.cpp
+++ src/sector_times.cpp
@@ -1,6 +1,9 @@
 #include "ros/ros.h"
 #include "sensor_msgs/NavSatFix.h"
 #include "first_project/custom.h"
+#include "geometry_msgs/PointStamped.h"
+#include <cmath>
+#include <deque>
 
 class SectorTimer {
 public:
@@ -19,6 +22,9 @@ public:
         private_n.param("sector3_longit", sec3_longit, 9.280767);
 
         last_time = ros::Time::now();
+
+        max_distance_threshold_ = 1;
+        window_size_ = 5;
     }
 
 private:
@@ -34,10 +40,33 @@ private:
     ros::Time last_time;
     double v_average;
     int count;
-    int current_sector;
+    int current_sector=1;
     double current_speed;
 
-    const double tol = 0.00005; // tolleranza per confronto coordinate
+    const double tol = 0.0005; // tolleranza per confronto coordinate
+
+    double max_distance_threshold_;
+    int window_size_;
+    std::deque<std::pair<double, double>> valid_points_; // lat, lon
+
+    void addValidPoint(double lat, double lon)
+    {
+        if (valid_points_.size() >= window_size_) {
+            valid_points_.pop_front();
+        }
+        valid_points_.emplace_back(lat, lon);
+    }
+
+    std::pair<double, double> computeAverage()
+    {
+        double sum_lat = 0.0, sum_lon = 0.0;
+        for (const auto& pt : valid_points_) {
+            sum_lat += pt.first;
+            sum_lon += pt.second;
+        }
+        int n = valid_points_.size();
+        return { sum_lat / n, sum_lon / n };
+    }
 
     void speedCallback(const geometry_msgs::PointStamped::ConstPtr& msg) {
         current_speed = msg->point.y;
@@ -48,33 +77,53 @@ private:
         double longit = msg->longitude;
         ros::Time now = ros::Time::now();
 
+        if (valid_points_.empty()) {
+            addValidPoint(lat, longit);
+            
+            
+        }
+
+        auto last = valid_points_.back();
+
+        if (abs(lat - last.first) > max_distance_threshold_ || abs(longit - last.second) > max_distance_threshold_){
+            ROS_WARN("GPS outlier detected . Using average fallback.");
+            auto avg = computeAverage();
+            lat = avg.first;
+            longit = avg.second;
+            
+        } else {
+            addValidPoint(lat, longit);
+            
+        }
+        
+        
         // Settore 1
         if (std::abs(lat - sec1_lat) < tol && std::abs(longit - sec1_longit) < tol) {
-            if (current_sector != 1) {
+            if (current_sector == 1) {
                 last_time = now;
                 v_average = 0;
                 count = 0;
-                current_sector = 1;
+                current_sector = 2;
                 ROS_INFO("Entrato nel settore 1");
             }
         }
         // Settore 2
         else if (std::abs(lat - sec2_lat) < tol && std::abs(longit - sec2_longit) < tol) {
-            if (current_sector != 2) {
+            if (current_sector == 2) {
                 last_time = now;
                 v_average = 0;
                 count = 0;
-                current_sector = 2;
+                current_sector = 3;
                 ROS_INFO("Entrato nel settore 2");
             }
         }
         // Settore 3
         else if (std::abs(lat - sec3_lat) < tol && std::abs(longit - sec3_longit) < tol) {
-            if (current_sector != 3) {
+            if (current_sector == 3) {
                 last_time = now;
                 v_average = 0;
                 count = 0;
-                current_sector = 3;
+                current_sector = 1;
                 ROS_INFO("Entrato nel settore 3");
             }
         }
@@ -92,3 +141,10 @@ private:
     }
 };
 
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "sector_times");
+    SectorTimer sector_timer;
+    ros::spin();
+    return 0;
+}
